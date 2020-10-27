9468e18620a77c7689a4988447d0cb4ff01a96d1
kabrate <937160666@qq.com>
2020-10-27 19:41:44 +0800

    rename

--- stft/stft.py
+++ stft/stft.py
@@ -7,12 +7,13 @@ from matplotlib import pyplot as plt
 import numpy as np 
 amp = 2 * np.sqrt(2)
 #采样率 帧长度#
-rate,x = scipy.io.wavfile.read('C:/Users/jing/Desktop/语音比赛/quiteroom_echo0_11.wav')
+rate,x = scipy.io.wavfile.read('C:/Users/jing/Desktop/语音比赛/resampled.wav')
 f,t,z=scipy.signal.stft(x, fs =rate,window ='hann')
 
 plt.pcolormesh(t,f,np.abs(z), vmin = 0, vmax = amp)
 plt.title('STFT Magnitude')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
-plt.show()
+#plt.show()
+plt.savefig('stft.png')
 
