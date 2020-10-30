96bc614f6dcadde30c4ec8326f18a79ddb430e66
kabrate <937160666@qq.com>
2020-10-30 21:01:50 +0800

    add melspectrogram

--- spectrogram/melspectrogram.py
+++ spectrogram/melspectrogram.py
@@ -4,12 +4,13 @@ import numpy as np
 import librosa.display
 
 waveform, sr=librosa.core.load("C:/Users/jing/Desktop/语音比赛/waveform.wav",sr=None)
-win_length = int(0.025 * sr)  # Window length 15ms, 25ms, 50ms, 100ms, 200ms
-hop_length = int(0.010 * sr)  # Window shift  10ms
+win_length = int(0.025 * sr)  # Window length 15ms, 25ms, 50ms, 100ms, 200ms 音频帧长度
+hop_length = int(0.010 * sr)  # Window shift  10ms 音频帧间隔
 n_fft = win_length
 S = librosa.feature.melspectrogram(waveform, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=128)
 S_DB = librosa.power_to_db(S, ref=np.max)
-plt.figure(figsize=(15, 6));
-librosa.display.specshow(S_DB, sr=sr, hop_length=512, x_axis='time', y_axis='mel');
+plt.figure(figsize=(12, 6));
+librosa.display.specshow(S_DB, sr=sr, y_axis='linear');
 plt.colorbar(format='%+2.0f dB');
 plt.show();
+#语谱图也叫时频谱图 横轴是时间 纵轴是频率  颜色表示幅度 声强 
--- spectrogram/spectrogram.py
+++ spectrogram/spectrogram.py
@@ -12,12 +12,12 @@ filename =filepath+'waveform.wav'
 y, sr = librosa.load(filename,sr=None)
  #调整大小
 plt.figure(figsize=(12, 6))
-#幅度转化为幅度的平方  功率谱
+#振幅转化为分贝
 D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
 #plt.subplot(1, 1, 1)
 #标签
 librosa.display.specshow(D, y_axis='linear')
-#分贝
+#分贝颜色
 plt.colorbar(format='%+2.0f dB')
-plt.title('Linear-frequency power spectrogram')
+plt.title('Linear-frequency power spectrogram')#线性频率功率谱图
 plt.show()
